55149b876f73028ff2793dd97f6d21c840c610da
Alex Davis <alex@alexs-imac.lan>
2017-09-24 17:16:45 +0100

    Add solutions for problem 5

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,20 @@
 #write your code here
+def translate(a)
+	vowels = ["a", "e", "i", "o", "u", "y"] #not sure if y is a vowel
+	result = ""
+	words = a.split
+	words.each do |i|
+		temp = i
+		while !vowels.include?(temp[0])
+			if temp[0] == "q" #probably more efficiant to have this after while, but doesnt really matter
+				temp = temp[2..-1]+"qu"
+			else
+				first = temp[0]
+				temp = temp[1..-1]+first
+			end
+		end
+		result += temp+"ay "		
+	end
+	return result.rstrip
+		
+end
