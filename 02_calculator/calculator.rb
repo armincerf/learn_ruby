2c2767a39d01a9abee4b698b5e224ebf2ae90f1f
Alex Davis <alex@alexs-imac.lan>
2017-09-24 14:58:35 +0100

    Add solutions for first 3 problems

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,30 @@
 #write your code here
+def add(a, b)
+	return a+b
+end
+def subtract(a,b)
+	return a-b
+end
+def sum(a)
+	b = 0
+	a.each do |i|
+		b +=i
+	end
+	return b
+end
+def multiply(*args)
+	total = 1
+	args.each do |i|
+		total *= i
+	end
+	return total
+end
+def power (a,b)
+	return a**b
+end
+def factorial(a)
+	if a==0
+		return 1
+	end
+	return a*factorial(a-1)
+end
